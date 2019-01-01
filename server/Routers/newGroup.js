ad26976e1de6855e6e29c3a2f4d41105e7442b08
efratlev <38768943+efratlev@users.noreply.github.com>
2019-01-01 19:54:56 +0200

    server

---
+++ server/Routers/newGroup.js
@@ -0,0 +1,25 @@
+﻿import
+
+router.route('/newGroup').post(function (req, res){
+    group.create({ groupName: req.body.groupName,
+    }).exec(function (err, newGroup) {
+        if (err){
+            console.error('GET Error: There was a problem retrieving: ' + err);
+            res.status(err.statusCode || 500).json(err);
+        }
+        else{
+            user.update({_id:req.body.userId},
+                {groups:{$push:{groupId:newGroup._id,authorizationId:'manager_id'}}
+                }).exec(function (err,userAsManager){
+                    if(err){
+                        console.error('GET Error: There was a problem retrieving: ' + err);
+                        res.status(err.statusCode || 500).json(err);
+                    }
+                    else{
+                        res.json(user);
+                    }
+                })
+            res.json(group);
+        }
+    });
+    });
---
+++ server/Routers/newTask.js
@@ -0,0 +1,33 @@
+﻿
+router.route('/newTask').post(function (req, res) {
+    user.findOne({
+        _id: req.body.UserId,authorizationId:'not 1'
+    }).exec(function (err, newTask) {
+        if (err) {
+            console.error('GET Error: There was a problem retrieving: ' + err);
+            res.status(err.statusCode || 500).json(err);
+        }
+        else {
+            task.create({
+                taskName: req.body.taskName,
+                description: req.body.description,
+                dateCreated: req.body.dateCreated,
+                assignedTo: req.body.assignedId,
+                addedBy: req.body.addedBy,
+                priority: req.body.priority,
+                frequency: req.body.frequency,
+                status: req.body.status,
+                comments: req.body.comments,
+                businessName: req.body.businessName,
+            }).exec(function (err, newTask) {
+                if (err) {
+                    console.error('GET Error: There was a problem retrieving: ' + err);
+                    res.status(err.statusCode || 500).json(err);
+                }
+                else {
+                        res.json(task);
+                }
+            })
+        }
+    })
+});
